b2ab8f0f21abfcdd1cc5abbed23a3971397ef819
Даниэль Агирре Кабрера <agirrekabrera.d@cet-mipt.ru>
2024-02-15 11:14:18 +0300

    master: Test for PUT

---
+++ internal/transport/rest/service-handler_test.go
@@ -0,0 +1,50 @@
+package rest
+
+import (
+	"FriendsAdvice/internal/services"
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGet(t *testing.T) {
+	r := CreateRouter(&services.Controller{})
+	ts := httptest.NewServer(r.MuxRouter)
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL + "/objects/1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Status code for /GET/1 is wrong. Have: %d, want: %d.", res.StatusCode, http.StatusOK)
+	}
+}
+
+func TestPut(t *testing.T) {
+	r := CreateRouter(&services.Controller{})
+	ts := httptest.NewServer(r.MuxRouter)
+	defer ts.Close()
+
+	// Create request
+	obj := "Oh!"
+	reqBody := bytes.NewBufferString(obj)
+
+	req, err := http.NewRequest("PUT", ts.URL+"/objects/AnyKey", reqBody)
+	if err != nil {
+		t.Fatalf("Error creating PUT request: %v", err)
+	}
+
+	// Send
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("Error sending PUT request: %v", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Status code for /PUT/objects/AnyKey is wrong. Have: %d, want: %d.", res.StatusCode, http.StatusOK)
+	}
+}
