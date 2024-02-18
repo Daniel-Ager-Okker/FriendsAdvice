eee609a3deec4db8c89895555a8c20c97445809b
Daniel Aguirre Cabrera <agirrekabrera.daniel@bk.ru>
2024-02-18 21:06:00 +0300

    master: added prometheus docker step

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	github.com/prometheus/client_golang v1.18.0 // indirect
 )
--- internal/transport/rest/metrics-handler.go
+++ internal/transport/rest/metrics-handler.go
@@ -1,3 +1,8 @@
 package rest
 
-//
+// Function for getting metrics
+// func GetMetrics() http.HandlerFunc {
+// 	return func(w http.ResponseWriter, _ *http.Request) {
+// 		promhttp.Handler().ServeHTTP(c.Writer, c.Request)
+// 	}
+// }
